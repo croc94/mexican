d7ee49d0b6283b94eb5d3646826fda8ba074bbc5
croc94 <croc94@mail.ru>
2023-12-17 16:17:48 +0300

    create class Bullet

---
+++ bullet.py
@@ -0,0 +1,21 @@
+'''Модуль для описания класса Bullet'''
+
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    '''Класс для управления пулями, выпущенными кораблем'''
+    def __init__(self, game_settings, screen, ship,) -> None:
+        super().__init__()
+        self.screen = screen
+
+        #Создание пули в позиции (0,0)и назначение правильной позиции
+        self.rect = pygame.Rect (0, 0, game_settings.bullet_width, game_settings.bullet_height, )
+        self.rect.left = ship.rect.right
+        self.rect.top = ship.rect.centery
+
+        #Позиция пули хранится в вещественном формате
+        self.x = float (self.rect.x)
+
+        self.color = game_settings.bullet_color
+        self.speed_factor = game_settings.bullet_speed_factor
