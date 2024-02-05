f84b4cb96ac57b38db0f0970e15eda3040a3c6de
croc94 <croc94@mail.ru>
2024-02-05 18:25:54 +0300

    create Tacos class

---
+++ tacos.py
@@ -0,0 +1,27 @@
+'''Модуль для хранения кода пришельцев'''
+
+import pygame
+from pygame.sprite import Sprite
+
+class Tacos(Sprite):
+    '''Класс представляющий однин такос'''
+    def __init__(self, settings, screen) -> None:
+        '''Инициализирует пришельца и задает начальную позицию'''
+        super().__init__()
+        self.settings = settings
+        self.screen = screen
+
+        #Загрузка изображения тако и назначение аттрибута rect
+        self.image = pygame.image.load ('images/tacos.bmp')
+        self.rect = self.image.get_rect ()
+
+        #Каждый новый пришелец появляется в левом верхнем углу экрана
+        self.rect.x = self.settings.screen_width - 2 * self.rect.width
+        self.rect.y = self.rect.height
+
+        #Сохранение точной позиции тако
+        self.y = float (self.rect.y)
+    
+    def blitme (self):
+        '''Выводит тако в текущем положении'''
+        self.screen.blit (self.image, self.rect)
